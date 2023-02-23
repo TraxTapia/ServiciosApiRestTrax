3cf213ef77b8adcf7fba8b6a94f1ab5a345fb5c9
Ivan Tapia Avalos <ivan.tapia@mediaccess.com.mx>
2023-02-23 11:16:23 -0600

    se modificab las dll

---
+++ Trax.Models.ApiServicesWeb/Enum/EnumClaims.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trax.Models.ApiServicesWeb.Enum
+{
+    public enum EnumClaims
+    {
+        AccessToken = 0,
+        CodeProvider = 1,
+        NameProvider = 2,
+        RoleAdministrator = 3
+    }
+}
